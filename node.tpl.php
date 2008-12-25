69954dcaa0f255ca0abf6c984529e22a7e2506d1
Anton Sidashin <restyler@24628.no-reply.drupal.org>
2008-12-25 13:33:29 +0000

    [+] O GOD

--- node.tpl.php
+++ node.tpl.php
@@ -11,7 +11,7 @@
     <?php if ($page == 0) { ?><h2 class="title"><a href="<?php print $node_url?>"><?php print $title?></a></h2><?php }; ?>
    
     
-    <div class="content"><?php print $content?></div>
+    <div class="content clear-block"><?php print $content?></div>
 	<?php if ($terms): ?><div class="taxonomy"><?php print $terms?></div><?php endif; ?>
     <?php if ($links) { ?><div class="links">&raquo; <?php print $links?></div><?php }; ?>
   </div>
