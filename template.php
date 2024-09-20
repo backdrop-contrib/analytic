a576900353bb5c82faf471ed6a744295c6cf7e44
Jen Lampton <jen@jenerationweb.com>
2024-09-20 16:43:56 -0700

    Updates to block template and preprocess. Home page now loads.

--- template.php
+++ template.php
@@ -4,37 +4,12 @@
  */
 
 /**
- * Implementation of hook_menu_local_task().
- */
-function analytic_menu_local_task($variables) {
-  $link = $variables['element']['#link'];
-  $link_text = $link['title'];
-  $link['localized_options']['html'] = TRUE;
-
-  if (!empty($variables['element']['#active'])) {
-    // Add text to indicate active tab for non-visual users.
-    $active = '<span class="element-invisible">' . t('(active tab)') . '</span>';
-
-    // If the link does not contain HTML already, check_plain() it now.
-    // After we set 'html'=TRUE the link will not be sanitized by l().
-    if (empty($link['localized_options']['html'])) {
-      $link['title'] = check_plain($link['title']);
-    }    
-    $link_text = t('!local-task-title !active', array('!local-task-title' => $link['title'], '!active' => $active));    
-  }
-  
-  $link_text = '<span class="tab">' . $link_text . '</span>';
- 
-  return '<li' . (!empty($variables['element']['#active']) ? ' class="active"' : '') . '>' . l($link_text, $link['href'], $link['localized_options']) . "</li>\n";
-}
-
-
-/**
  * Prepares variables for page template files.
  *
  * @see page.tpl.php
  */
-function analytic_process_page(&$variables) {
+function analytic_preprocess_page(&$variables) {
+  //dpm('hello');
   if (empty($variables['page_bottom'])) {
     $variables['page_bottom'] = '';
   }
@@ -45,4 +20,20 @@ function analytic_process_page(&$variables) {
   $variables['page_bottom'] .= '<span class="developer">';
   $variables['page_bottom'] .= '  <strong>' . $copy . '</strong>';
   $variables['page_bottom'] .= '</span>';
+}
+/**
+ * Prepare variables for block templates.
+ *
+ * @see block.tpl.php
+ */
+function analytic_preprocess_block(&$variables) {
+  // Classes is already a string here, so add block-[module].
+  $variables['classes'] .= ' block-' . $variables['block']->module;
+}
+
+/**
+ * Implementation of hook_menu_local_task().
+ */
+function analytic_menu_local_task($variables) {
+  // There is no hook_menu_local_task in Drupal 7 or Backdrop.
 }
--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -1,9 +1,26 @@
-<?php
-/**
- * Template for Blocks.
- */
-?>
-  <div class="block block-<?php print $block->module; ?> clearfix" id="block-<?php print $block->module; ?>-<?php print $block->delta; ?>">
-   <?php if ($block->subject) { ?><h2 class="title"><?php print $block->subject; ?></h2><?php } ?>
-    <div class="content clearfix"><?php print $content; ?></div>
+ <?php
+ /**
+  * @file
+  * Template for outputting the default block styling within a Layout.
+  *
+  * Variables available:
+  * - $classes: STRING of classes that should be displayed on the block's wrapper.
+  * - $title: The title of the block.
+  * - $title_prefix/$title_suffix: A prefix and suffix for the title tag. This
+  *   is important to print out as administrative links to edit this block are
+  *   printed in these variables.
+  * - $content: The actual content of the block.
+  */
+ ?>
+<div class="<?php print $classes; ?>" <?php print $attributes; ?>>
+
+ <?php print render($title_prefix); ?>
+ <?php if ($title): ?>
+   <h2 class="block-title"><?php print $title; ?></h2>
+ <?php endif; ?>
+ <?php print render($title_suffix); ?>
+
+   <div class="block-content content clearfix">
+     <?php print render($content); ?>
+   </div>
  </div>
